59e26ede88deecf3a58d0dd9112af38a180e6394
Moiz Merchant <moiz@tiktok.com>
2011-12-15 17:30:19 -0800

    Added google map example.

--- src/com/tiktok/consumerapp/TikTokActivity.java
+++ src/com/tiktok/consumerapp/TikTokActivity.java
@@ -11,17 +11,18 @@ package com.tiktok.consumerapp;
 import java.util.List;
 
 import android.app.Activity;
-import android.database.Cursor;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.location.Location;
 import android.location.LocationManager;
 import android.location.LocationListener;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 //-----------------------------------------------------------------------------
 // class implementation
@@ -139,6 +140,16 @@ public class TikTokActivity extends Activity
         super.onDestroy();
         if (mDatabaseAdapter != null) mDatabaseAdapter.close();
         if (mCursor != null) mCursor.close();
+    }
+
+    //-------------------------------------------------------------------------
+    // events
+    //-------------------------------------------------------------------------
+
+    public void openMap(View view)
+    {
+        Intent mapIntent = new Intent(this, TikTokMapActivity.class);
+        startActivity(mapIntent);
     }
 
     //-------------------------------------------------------------------------
---
+++ src/com/tiktok/consumerapp/TikTokMapActivity.java
@@ -0,0 +1,159 @@
+//-----------------------------------------------------------------------------
+// MapActivity
+//-----------------------------------------------------------------------------
+
+package com.tiktok.consumerapp;
+
+//-----------------------------------------------------------------------------
+// imports
+//-----------------------------------------------------------------------------
+
+import android.os.Bundle;
+//import android.widget.LinearLayout;
+import android.view.KeyEvent;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+//import com.google.android.maps.MapView.LayoutParams;
+
+//-----------------------------------------------------------------------------
+// class implementation
+//-----------------------------------------------------------------------------
+
+public class TikTokMapActivity extends MapActivity 
+{
+    
+    /**
+     * Called when the activity is first created. 
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+
+        // grab the map view 
+        mMapView = (MapView)findViewById(R.id.map_view);
+
+        // add the map's zoom view to the zoom layout
+        mMapView.setBuiltInZoomControls(true);
+        /*
+        LinearLayout zoomLayout = (LinearLayout)findViewById(R.id.zoom);
+        zoomLayout.addView(mMapView.getZoomControls(),
+            new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT));
+        mMapView.displayZoomControls(true);
+        */
+
+        // zoom in on a specific location
+        int latitude        = (int)(49.27697 * 1e6);
+        int longitude       = (int)(-123.01148 * 1e6);
+        GeoPoint coordinate = new GeoPoint(latitude, longitude);
+
+        MapController mapController = mMapView.getController();
+        mapController.animateTo(coordinate);
+        mapController.setZoom(17);
+        mMapView.invalidate();
+    }
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * The activity is about to become visible.
+     */
+    @Override
+    protected void onStart() 
+    {
+        super.onStart();
+    }
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * The activity has become visible (it is now "resumed").
+     */
+    @Override
+    protected void onResume() 
+    {
+        super.onResume();
+    }
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * Another activity is taking focus (this activity is about to be "paused")
+     */
+    @Override
+    protected void onPause() 
+    {
+        super.onPause();
+    }
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * The activity is no longer visible (it is now "stopped")
+     */
+    @Override
+    protected void onStop() 
+    {
+        super.onStop();
+    }
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * The activity is about to be destroyed.
+     */
+    @Override
+    protected void onDestroy() 
+    {
+        super.onDestroy();
+    }
+
+    //-------------------------------------------------------------------------
+    // Events
+    //-------------------------------------------------------------------------
+
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        MapController mapController = mMapView.getController();
+        switch (keyCode)
+        {
+            case KeyEvent.KEYCODE_3:
+                mapController.zoomIn();
+                break;
+            case KeyEvent.KEYCODE_1:
+                mapController.zoomOut();
+                break;
+        }
+
+        // switch to satellite view
+        //mMapView.setSatellite(true);
+
+        // swtich to street view
+        //mMapView.setStreetView(true);
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    //-------------------------------------------------------------------------
+    // MapsActivity 
+    //-------------------------------------------------------------------------
+
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+
+    //-------------------------------------------------------------------------
+    // fields
+    //-------------------------------------------------------------------------
+
+    private MapView mMapView;
+    
+}
+
