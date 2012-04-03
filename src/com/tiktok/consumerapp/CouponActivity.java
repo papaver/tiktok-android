4a46f7ab1149948d44a9c33ee7c69572b368c667
Moiz Merchant <moiz@tiktok.com>
2012-04-02 18:13:11 -0700

    Inital pass at coupon activity layout.

--- src/com/tiktok/consumerapp/CouponActivity.java
+++ src/com/tiktok/consumerapp/CouponActivity.java
@@ -9,16 +9,29 @@ package com.tiktok.consumerapp;
 //-----------------------------------------------------------------------------
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-//import android.util.Log;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.maps.MapActivity;
 
 //-----------------------------------------------------------------------------
 // class implementation
 //-----------------------------------------------------------------------------
 
-public class CouponActivity extends Activity
+public class CouponActivity extends MapActivity
 {
+    //-------------------------------------------------------------------------
+    // statics
+    //-------------------------------------------------------------------------
+
+    private static final String kLogTag = "CouponActivity";
+
+    //-------------------------------------------------------------------------
+    // activity
+    //-------------------------------------------------------------------------
 
     /**
      * Called when the activity is first created.
@@ -27,10 +40,7 @@ public class CouponActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
-        TextView textView = new TextView(this);
-        textView.setText("This is the Coupon Activity.");
-        setContentView(textView);
+        setContentView(R.layout.coupon);
     }
 
     //-------------------------------------------------------------------------
@@ -87,5 +97,50 @@ public class CouponActivity extends Activity
     {
         super.onDestroy();
     }
+
+    //-------------------------------------------------------------------------
+    // Events
+    //-------------------------------------------------------------------------
+
+    public void onClickMerchant(View view)
+    {
+        Intent intent = new Intent(this, MerchantActivity.class);
+        intent.putExtra(MerchantTable.sKeyId, 0);
+        startActivityForResult(intent, 0);
+    }
+
+    //-------------------------------------------------------------------------
+
+    public void onClickTwitter(View view)
+    {
+        Log.i(kLogTag, "Share Twitter");
+    }
+
+    //-------------------------------------------------------------------------
+
+    public void onClickFacebook(View view)
+    {
+        Log.i(kLogTag, "Share Facebook");
+    }
+
+    //-------------------------------------------------------------------------
+
+    public void onClickMore(View view)
+    {
+        Log.i(kLogTag, "Share More");
+    }
+
+    //-------------------------------------------------------------------------
+    // MapsActivity
+    //-------------------------------------------------------------------------
+
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
+    }
+
+    //-------------------------------------------------------------------------
+    // fields
+    //-------------------------------------------------------------------------
 }
 
