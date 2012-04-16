5a7f377c731c6401d87571b2ec2049f000939320
Moiz Merchant <moiz@tiktok.com>
2012-04-16 12:39:53 -0500

    Fading deal out when expired in list view.

--- src/com/tiktok/consumerapp/CouponAdapter.java
+++ src/com/tiktok/consumerapp/CouponAdapter.java
@@ -18,10 +18,9 @@ import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
+import android.view.animation.AlphaAnimation;
 import android.widget.CursorAdapter;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -29,6 +28,7 @@ import android.util.Log;
 
 import com.tiktok.consumerapp.drawable.BitmapDrawable;
 import com.tiktok.consumerapp.utilities.TextUtilities;
+import com.tiktok.consumerapp.utilities.UIUtilities;
 
 //-----------------------------------------------------------------------------
 // class implementation
@@ -85,7 +85,13 @@ public final class CouponAdapter extends CursorAdapter
         // query merchant from cursor
         final Merchant merchant = adapter.fetchMerchant(merchantId);
 
-        // setting the title view is strait forward
+        // reset alpha
+        AlphaAnimation alpha = new AlphaAnimation(1.0f, 1.0f);
+        alpha.setDuration(0);
+        alpha.setFillAfter(true);
+        viewHolder.main.startAnimation(alpha);
+
+        // set merchant / title / expires at
         viewHolder.merchant.setText(merchant.name().toUpperCase());
         viewHolder.title.setText(TextUtilities.capitalizeWords(title));
         viewHolder.expiresTime.setText(Coupon.getExpirationTime(endTime));
@@ -110,19 +116,85 @@ public final class CouponAdapter extends CursorAdapter
         }
 
         // setup timer
-        mHandler.removeCallbacks(viewHolder.timer);
-        viewHolder.timer = new Runnable() {
-           public void run() {
-                viewHolder.expiresTime.setText(Coupon.getExpirationTime(endTime));
-                GradientDrawable background = (GradientDrawable)viewHolder.linearLayout.getBackground();
-                background.setColor(Coupon.getColor(endTime, startTime));
-                mHandler.postDelayed(this, 1000);
-           }
-        };
-        mHandler.post(viewHolder.timer);
+        setupTimer(viewHolder, startTime, endTime);
 
         // set icon image
+        setupIcon(viewHolder, iconId, iconUrl);
+
+        // cleanup
+        adapter.close();
+    }
+
+    //-------------------------------------------------------------------------
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent)
+    {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        final View view = inflater.inflate(R.layout.coupon_entry_list_item, parent, false);
+        return view;
+    }
+
+    //-------------------------------------------------------------------------
+
+    private void setupTimer(final ViewHolder viewHolder,
+        final Date startTime, final Date endTime)
+    {
+        // clear current timer from loop
+        mHandler.removeCallbacks(viewHolder.timer);
+
+        // no need to set timer if coupon is already expired
+        if (Coupon.isExpired(endTime)) {
+            expireCoupon(viewHolder, 0);
+            return;
+        }
+
+        // setup new timer
+        viewHolder.timer = new Runnable() {
+           public void run() {
+
+                // update expire time
+                viewHolder.expiresTime.setText(Coupon.getExpirationTime(endTime));
+
+                // update background color
+                GradientDrawable background = (GradientDrawable)viewHolder.linearLayout.getBackground();
+                background.setColor(Coupon.getColor(endTime, startTime));
+
+                // fade out coupon if expired
+                if (Coupon.isExpired(endTime)) {
+                    expireCoupon(viewHolder, 200);
+                } else {
+                    mHandler.postDelayed(this, 1000);
+                }
+           }
+        };
+
+        // run timer
+        mHandler.post(viewHolder.timer);
+    }
+
+    //-------------------------------------------------------------------------
+
+    public void expireCoupon(ViewHolder viewHolder, long milliseconds)
+    {
+        // fade out coupon
+        AlphaAnimation alpha = new AlphaAnimation(1.0f, 0.6f);
+        alpha.setDuration(milliseconds);
+        alpha.setFillAfter(true);
+        viewHolder.main.startAnimation(alpha);
+
+        // update expire time
+        viewHolder.expiresAt.setText("Offer is no longer available.");
+    }
+
+    //-------------------------------------------------------------------------
+
+    private void setupIcon(final ViewHolder viewHolder,
+        final int iconId, final String iconUrl)
+    {
         IconManager.IconData iconData = new IconManager.IconData(iconId, iconUrl);
+
+        // set image if available
         BitmapDrawable icon = mIconManager.getImage(iconData);
         if (icon != null) {
             viewHolder.icon.setImageBitmap(icon.getBitmap());
@@ -132,21 +204,7 @@ public final class CouponAdapter extends CursorAdapter
 
             // set activity indicator
             viewHolder.icon.setImageResource(R.drawable.activity_indicator);
-
-            RotateAnimation rotation = new RotateAnimation(
-                0.0f,
-                360.0f,
-                Animation.RELATIVE_TO_SELF,
-                0.5f,
-                Animation.RELATIVE_TO_SELF,
-                0.5f);
-            rotation.setDuration(360 * 4);
-            rotation.setInterpolator(new LinearInterpolator());
-            rotation.setRepeatMode(Animation.RESTART);
-            rotation.setRepeatCount(Animation.INFINITE);
-
-            // and apply it to your imageview
-            viewHolder.icon.startAnimation(rotation);
+            viewHolder.icon.startAnimation(UIUtilities.getActivityIndicatorAnimation());
 
             // download icon from server
             mIconManager.requestImage(iconData, new IconManager.CompletionHandler() {
@@ -166,19 +224,6 @@ public final class CouponAdapter extends CursorAdapter
                 }
             });
         }
-
-        // cleanup
-        adapter.close();
-    }
-
-    //-------------------------------------------------------------------------
-
-    @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent)
-    {
-        LayoutInflater inflater = LayoutInflater.from(context);
-        final View view = inflater.inflate(R.layout.coupon_entry_list_item, parent, false);
-        return view;
     }
 
     //-------------------------------------------------------------------------
@@ -191,6 +236,7 @@ public final class CouponAdapter extends CursorAdapter
      */
     private static class ViewHolder
     {
+        public FrameLayout  main;
         public TextView     merchant;
         public TextView     title;
         public TextView     expiresAt;
@@ -209,6 +255,7 @@ public final class CouponAdapter extends CursorAdapter
         final Object tag = view.getTag();
         if ((tag == null) || !(tag instanceof ViewHolder)) {
             ViewHolder holder   = new ViewHolder();
+            holder.main         = (FrameLayout)view.findViewById(R.id.coupon);
             holder.merchant     = (TextView)view.findViewById(R.id.coupon_merchant);
             holder.title        = (TextView)view.findViewById(R.id.coupon_title);
             holder.expiresAt    = (TextView)view.findViewById(R.id.coupon_expires_at);
