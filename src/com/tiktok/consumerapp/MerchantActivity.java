ae6572f6f76df92bb1b9b7b55886969cfd911177
Moiz Merchant <moiz@tiktok.com>
2012-08-03 11:39:21 -0400

    Added location table to merchant activity.

--- src/com/tiktok/consumerapp/MerchantActivity.java
+++ src/com/tiktok/consumerapp/MerchantActivity.java
@@ -8,19 +8,25 @@ package com.tiktok.consumerapp;
 // imports
 //-----------------------------------------------------------------------------
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.text.util.Linkify;
-import android.text.util.Linkify.TransformFilter;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+import android.widget.ExpandableListView.OnGroupCollapseListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.util.Log;
 
 import com.tiktok.consumerapp.drawable.BitmapDrawable;
 import com.tiktok.consumerapp.utilities.ColorUtilities;
@@ -189,6 +195,71 @@ public class MerchantActivity extends Activity
 
         // icon
         setupIcon(merchant);
+
+        // setup locations table
+        if (multipleLocations) {
+            setupLocationsTable(coupon);
+        } else {
+            ExpandableListView locationListView =
+                (ExpandableListView)findViewById(R.id.locationlist);
+            ViewGroup.LayoutParams params = locationListView.getLayoutParams();
+            params.height = locationListView.getMeasuredHeight();
+            locationListView.setLayoutParams(params);
+        }
+    }
+
+    //-------------------------------------------------------------------------
+
+    private void setupLocationsTable(final Coupon coupon)
+    {
+        // create a new list adapter
+        mListAdapter = new ExpadableLocationAdapter(this,
+            coupon.merchant(), coupon.locations());
+
+        // get window metrics
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        // setup the list view
+        final ExpandableListView locationListView =
+            (ExpandableListView)findViewById(R.id.locationlist);
+        locationListView.setAdapter(mListAdapter);
+        locationListView.setIndicatorBounds(
+            metrics.widthPixels - GetDipsFromPixel(40),
+            metrics.widthPixels - GetDipsFromPixel(20));
+
+        // calculate group view height
+        View groupView = mListAdapter.newGroupView(this, null);
+        groupView.measure(0, 0);
+        final int groupHeight = groupView.getMeasuredHeight();
+
+        // calculate child view height
+        View childView = mListAdapter.newChildView(this, null);
+        childView.measure(0, 0);
+        final int childHeight = childView.getMeasuredHeight();
+
+        // resize list view to just its header
+        ViewGroup.LayoutParams params = locationListView.getLayoutParams();
+        params.height = groupHeight;
+        locationListView.setLayoutParams(params);
+
+        // on expand update the layout and shift details view down
+        locationListView.setOnGroupExpandListener(new OnGroupExpandListener() {
+            public void onGroupExpand(int groupPosition) {
+                ViewGroup.LayoutParams params = locationListView.getLayoutParams();
+                params.height = groupHeight + childHeight * mListAdapter.getChildrenCount(0);
+                locationListView.setLayoutParams(params);
+            }
+        });
+
+        // on collapse update the layout and shift details view up
+        locationListView.setOnGroupCollapseListener(new OnGroupCollapseListener() {
+            public void onGroupCollapse(int groupPosition) {
+                ViewGroup.LayoutParams params = locationListView.getLayoutParams();
+                params.height = groupHeight;
+                locationListView.setLayoutParams(params);
+            }
+        });
     }
 
     //-------------------------------------------------------------------------
@@ -199,27 +270,8 @@ public class MerchantActivity extends Activity
         String splitAddress  = TextUtils.join("\n", address.split(", ", 2));
         addressView.setText(splitAddress);
 
-        // [moiz] fucking canadain addresses aren't recognized?!
-        if (addressView.getUrls().length == 0) {
-
-            // reset the link mask so we can use custom links
-            addressView.setAutoLinkMask(0);
-
-            // transform the address to be url compliant
-            TransformFilter filter = new TransformFilter() {
-                public String transformUrl(Matcher match, String url) {
-                    String formatted = url.replace("\n", ",+")
-                                          .replace(", ", ",+")
-                                          .replace( " ",  "+");
-                    return formatted;
-                }
-            };
-
-            // linkify the address
-            String scheme   = "geo:0,0?q=";
-            Pattern pattern = Pattern.compile("(.+)", Pattern.DOTALL);
-            Linkify.addLinks(addressView, pattern, scheme, null, filter);
-        }
+        // make sure address link mask exists
+        UIUtilities.addAddressLinkMask(addressView);
     }
 
     //-------------------------------------------------------------------------
@@ -268,10 +320,203 @@ public class MerchantActivity extends Activity
     }
 
     //-------------------------------------------------------------------------
+
+    public int GetDipsFromPixel(float pixels)
+    {
+        final float scale = getResources().getDisplayMetrics().density;
+        return (int) (pixels * scale + 0.5f);
+    }
+
+    //-------------------------------------------------------------------------
     // fields
     //-------------------------------------------------------------------------
 
-    private Coupon      mCoupon;
-    private IconManager mIconManager;
+    private Coupon                   mCoupon;
+    private IconManager              mIconManager;
+    private ExpadableLocationAdapter mListAdapter;
+
+}
+
+//-----------------------------------------------------------------------------
+// ExpadableLocationAdapter
+//-----------------------------------------------------------------------------
+
+final class ExpadableLocationAdapter extends BaseExpandableListAdapter
+{
+    //-------------------------------------------------------------------------
+    // constructor
+    //-------------------------------------------------------------------------
+
+    public ExpadableLocationAdapter(Context context, Merchant merchant,
+                                    List<Location> locations)
+    {
+        mContext   = context;
+        mMerchant  = merchant;
+        mLocations = locations;
+    }
+
+    //-------------------------------------------------------------------------
+    // overrides
+    //-------------------------------------------------------------------------
+
+    public int getGroupCount()
+    {
+        return 1;
+    }
+
+    //-------------------------------------------------------------------------
+
+    public long getGroupId(int groupPosition)
+    {
+        return groupPosition;
+    }
+
+    //-------------------------------------------------------------------------
+
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent)
+    {
+        View groupView = convertView == null ?
+            newGroupView(mContext, parent) : convertView;
+        TextView title = (TextView)groupView.findViewById(R.id.list_header_title);
+        title.setText(isExpanded ? "Close" : "Expand to see all locations");
+        return groupView;
+    }
+
+    //-------------------------------------------------------------------------
+
+    public Object getChild(int groupPosition, int childPosition)
+    {
+        return mLocations.get(childPosition);
+    }
+
+    //-------------------------------------------------------------------------
+
+    public long getChildId(int groupPosition, int childPosition)
+    {
+        return childPosition;
+    }
+
+    //-------------------------------------------------------------------------
+
+    public View getChildView(int groupPosition, int childPosition,
+                             boolean isLastChild, View convertView,
+                             ViewGroup parent)
+    {
+        View childView = convertView == null ?
+            newChildView(mContext, parent) : convertView;
+        bindChildView(childView, mContext, (Location)getChild(0, childPosition));
+        return childView;
+    }
+
+    //-------------------------------------------------------------------------
+
+    public int getChildrenCount(int groupPosition)
+    {
+        return mLocations.size();
+    }
+
+    //-------------------------------------------------------------------------
+
+    public Object getGroup(int groupPosition)
+    {
+        return mLocations;
+    }
+
+    //-------------------------------------------------------------------------
+
+    public boolean hasStableIds()
+    {
+        return true;
+    }
+
+    //-------------------------------------------------------------------------
+
+    public boolean isChildSelectable(int groupPosition, int childPosition)
+    {
+        return false;
+    }
+
+    //-------------------------------------------------------------------------
+    // helper functions
+    //-------------------------------------------------------------------------
+
+    public View newGroupView(Context context, ViewGroup parent)
+    {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        final View view = inflater.inflate(R.layout.list_header, parent, false);
+        return view;
+    }
+
+    //-------------------------------------------------------------------------
+
+    public View newChildView(Context context, ViewGroup parent)
+    {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        final View view = inflater.inflate(R.layout.location_list_item, parent, false);
+        return view;
+    }
+
+    //-------------------------------------------------------------------------
+
+    public void bindChildView(View view, Context context, Location location)
+    {
+        // retrieve corresponding ViewHolder, which optimizes lookup efficiency
+        final ViewHolder viewHolder = getViewHolder(view);
+
+        // format name / address
+        String name = location.name().equals("") ?
+            mMerchant.name().toUpperCase() : location.name();
+        String address = TextUtils.join("\n", location.address().split(", ", 2));
+
+        // set location data
+        viewHolder.name.setText(name);
+        viewHolder.address.setText(address);
+        viewHolder.phone.setText(location.phone());
+
+        // make sure address link mask exists
+        UIUtilities.addAddressLinkMask(viewHolder.address);
+    }
+
+    //-------------------------------------------------------------------------
+    // view holder
+    //-------------------------------------------------------------------------
+
+    /**
+     * ViewHolder allows us to avoid re-looking up view references
+     * Since views are recycled, these references will never change
+     */
+    private static class ViewHolder
+    {
+        public TextView name;
+        public TextView address;
+        public TextView phone;
+    }
+
+    //-------------------------------------------------------------------------
+
+    private ViewHolder getViewHolder(final View view)
+    {
+        // create a new holder if the tag is empty
+        final Object tag = view.getTag();
+        if ((tag == null) || !(tag instanceof ViewHolder)) {
+            ViewHolder holder = new ViewHolder();
+            holder.name       = (TextView)view.findViewById(R.id.name);
+            holder.address    = (TextView)view.findViewById(R.id.address);
+            holder.phone      = (TextView)view.findViewById(R.id.phone);
+            view.setTag(holder);
+        }
+
+        return (ViewHolder)view.getTag();
+    }
+
+    //-------------------------------------------------------------------------
+    // fields
+    //-------------------------------------------------------------------------
+
+    Context        mContext;
+    Merchant       mMerchant;
+    List<Location> mLocations;
+
 }
 
--- src/com/tiktok/consumerapp/utilities/UIUtilities.java
+++ src/com/tiktok/consumerapp/utilities/UIUtilities.java
@@ -8,9 +8,15 @@ package com.tiktok.consumerapp.utilities;
 // imports
 //-----------------------------------------------------------------------------
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import android.text.util.Linkify;
+import android.text.util.Linkify.TransformFilter;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.widget.TextView;
 //import android.util.Log;
 
 //-----------------------------------------------------------------------------
@@ -43,6 +49,32 @@ public final class UIUtilities
         rotation.setRepeatMode(Animation.RESTART);
         rotation.setRepeatCount(Animation.INFINITE);
         return rotation;
+    }
+
+    //-------------------------------------------------------------------------
+
+    public static void addAddressLinkMask(TextView textView)
+    {
+        if (textView.getUrls().length == 0) {
+
+            // reset the link mask so we can use custom links
+            textView.setAutoLinkMask(0);
+
+            // transform the address to be url compliant
+            TransformFilter filter = new TransformFilter() {
+                public String transformUrl(Matcher match, String url) {
+                    String formatted = url.replace("\n", ",+")
+                                          .replace(", ", ",+")
+                                          .replace( " ",  "+");
+                    return formatted;
+                }
+            };
+
+            // linkify the address
+            String scheme   = "geo:0,0?q=";
+            Pattern pattern = Pattern.compile("(.+)", Pattern.DOTALL);
+            Linkify.addLinks(textView, pattern, scheme, null, filter);
+        }
     }
 
     //-------------------------------------------------------------------------
